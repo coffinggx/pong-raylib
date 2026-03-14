329dcab5b6a2e72811679cba23dee27884993c51
Coffin <sudippaudel512@gmail.com>
2026-03-14 09:14:33 +0545

    feat: added game logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
 #include <raylib.h>
 #include "config.hpp"
-#include<iostream>
 #include "ball.hpp"
 #include "slider.hpp"
 int main() 
 {
     InitWindow(width,height, "Raylib program");
-    SetTargetFPS(1000);
+    SetTargetFPS(60);
     Ball ball; 
-    Slider leftSlider = Slider(10.0f,  GetScreenWidth()/3,Type::LEFT);
-    Slider rightSlider = Slider(10.0f, GetScreenWidth()/3,Type::RIGHT);
+    Slider leftSlider = Slider(10.0f, 30.0f,Type::LEFT);
+    Slider rightSlider = Slider(10.0f,30.0f,Type::RIGHT);
     while (!WindowShouldClose())
     {
         leftSlider.moveSlider();
         rightSlider.moveSlider();
         int fps = GetFPS();
-        const char * fps_s= std::to_string(fps).c_str();
         BeginDrawing();
-            DrawText(fps_s, 14,0,16,WHITE);
+            DrawText(TextFormat("Player1: %d",4), 14,0,16,WHITE);
+            DrawText(TextFormat("Player2: %d",4), width - 100,0,16,WHITE);
             ball.drawBall(); 
             leftSlider.drawSlider();
             rightSlider.drawSlider();
-            ball.moveBall();
-            ClearBackground(Color{ 50, 175, 50, 255 });
+            DrawLine(width/2,0,width/2,height,WHITE);
+            ball.updateBall();
+            ClearBackground(Color{0, 0, 0, 255 });
+            ball.updateBasedOnSlider(leftSlider.getInfo());
+            ball.updateBasedOnSlider(rightSlider.getInfo());
         EndDrawing();
     }
     
