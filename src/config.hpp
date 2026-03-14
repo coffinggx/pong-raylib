df5fbd9bb7b4394684c05b6f6ca017dc90410c9c
Coffin <sudippaudel512@gmail.com>
2026-03-14 09:15:10 +0545

    update: updated configs

--- src/config.hpp
+++ src/config.hpp
@@ -1,11 +1,5 @@
 #pragma once
-constexpr int width = 900;
-constexpr int height = 450;
+constexpr int width = 1280;
+constexpr int height = 800;
 
-#define float f32
-#define double f64
-#define uint16_t u16
-#define uint32_t u32 
-#define int16_t i16 
-#define int32_t i32 
-#define uint64_t u64
+
